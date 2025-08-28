d028a037aa2909bcd500a9166713c4b602e76d44
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-08-28 12:49:57 +0200

    fix: table scrolling

--- src/uicomponents/AgGridWrapper.tsx
+++ src/uicomponents/AgGridWrapper.tsx
@@ -2,7 +2,7 @@ import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
 import { DataModel, TableNames } from '../../convex/_generated/dataModel';
 
 export const AgGridWrapper = <K extends DataModel[TableNames]['document']>({ ...props }: AgGridReactProps<K>) => (
-  <div className="h-full w-full overflow-hidden transition-all duration-400">
+  <div className="h-full w-full transition-all duration-400">
     <AgGridReact
       {...props}
       rowSelection={{
