37a6250efdde5767ceb8563bf2acc011c0021464
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-08-28 13:21:32 +0200

    fix: remove more overflow-hidden

--- src/data/GeneralTable.tsx
+++ src/data/GeneralTable.tsx
@@ -1,11 +1,13 @@
 import { TableFooter } from '../uicomponents/TableFooter';
 
-export const GeneralTable: React.FC<React.ComponentProps<'div'> & { addMethod?: () => void; selectedItemsCount: number }> = ({ children, addMethod, selectedItemsCount, ...props }) => (
+export const GeneralTable: React.FC<
+  React.ComponentProps<'div'> & { addMethod?: () => void; selectedItemsCount: number }
+> = ({ children, addMethod, selectedItemsCount, ...props }) => (
   <div
-    className="max-w-screen-2xl mx-auto px-4 pb-4 overflow-hidden transition-all duration-400 flex flex-col max-h-[calc(100svh-var(--header-height))] h-[100svh] gap-4"
+    className="max-w-screen-2xl mx-auto px-4 pb-4 transition-all duration-400 flex flex-col max-h-[calc(100svh-var(--header-height))] h-[100svh] gap-4"
     {...props}
   >
-    <div className="flex-1 w-full flex flex-row overflow-hidden">{children}</div>
+    <div className="flex-1 w-full flex flex-row">{children}</div>
     <TableFooter addMethod={addMethod} selectedItemsCount={selectedItemsCount} />
   </div>
 );
