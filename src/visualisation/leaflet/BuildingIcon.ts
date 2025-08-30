93a94cae47cafc7f66e844f0ff0eaadbfb78ff8a
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-08-30 11:31:13 +0200

    feat: circrete map icon

---
+++ src/visualisation/leaflet/BuildingIcon.ts
@@ -0,0 +1,8 @@
+import { icon } from 'leaflet';
+import circreteLogo from '../../assets/circrete-logo.avif';
+
+export const CircreteIcon = icon({
+  iconUrl: circreteLogo,
+  iconSize: [32, 32],
+  iconAnchor: [16, 16]
+});
