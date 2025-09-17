c82b92c027f15c9b36683c8c9e7a0c1c44bde79e
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-17 18:10:22 +0200

    feat: app -> landing content

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,4 @@
-export const App = () => (
-  <main className="max-w-screen-2xl mx-auto p-4 bg-slate-800">
-    <header>
-      <h1>circrete</h1>
-      <p className="subtitle">Building Tomorrow's Cities with Yesterday's Concrete</p>
-    </header>
+import { ContentStyleWrapper } from './content/ContentStyleWrapper';
+import { Landing } from './content/Landing';
 
-    <section className=" flex flex-col gap-4">
-      <div className="flex flex-col gap-4">
-        <h3 className="text-xl font-bold">
-          circrete operates as a consultant for building owners, demolition companies and architects.
-        </h3>
-
-        <div className="flex flex-col gap-2">
-          <h2>We offer:</h2>
-          <ul className="flex flex-col gap-2">
-            <li>
-              <strong>Initial reuse assessment</strong> (prior to demolition) on-site and via the digital building
-              archive
-            </li>
-            <li>
-              <strong>Dismantling plans</strong>
-            </li>
-            <li>
-              <strong>Non-destructive testing</strong> of concrete elements in the donor structure
-            </li>
-            <li>
-              <strong>Labeling of elements</strong> with RFID chips that store information on the element history and
-              material quality to facilitate safe reintegration
-            </li>
-            <li>
-              <strong>Showcasing the reclaimed elements</strong> on our platform to find a receiver building
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  </main>
-);
+export const App = () => <ContentStyleWrapper children={<Landing />} />;
