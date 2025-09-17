c98492064d82416a97603804af8e6b44c1a88d10
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-17 22:27:45 +0200

    feat: some drop

--- src/content/ContentStyleWrapper.tsx
+++ src/content/ContentStyleWrapper.tsx
@@ -1,7 +1,7 @@
 export const ContentStyleWrapper: React.FC<React.ComponentProps<'div'>> = ({ children }) => {
   return (
     <div className="max-w-screen-2xl mx-auto px-4">
-      <div className="p-4 overflow-hidden rounded-lg bg-slate-800">{children}</div>
+      <div className="p-4 overflow-hidden rounded-lg bg-[#1d293d88] backdrop-blur-xs">{children}</div>
     </div>
   );
 };
